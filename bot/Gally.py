dccf2b40304689cfd95300c6364fb7e2e656e514
horotopia <horotopia@hotmail.com>
2020-12-16 04:35:18 +0100

    Update Gally.py

--- bot/Gally.py
+++ bot/Gally.py
@@ -72,14 +72,32 @@ async def on_message(message):
 			b = int(ListElementInMessage[2])+1000
 			c = round(((a * 3.04)+10000) / (1 - (b / (b + 1200))))
 			d = int((a*3.04)+10000)
-			embed = discord.Embed(title="Ce que donne 3 gemmes PV", color=0xffffff)
+			embed = discord.Embed(title="Ce que donnent 3 gemmes PV", color=0xffffff)
 			embed.set_thumbnail(url="https://vignette.wikia.nocookie.net/mslgame/images/a/aa/Gem.png/revision/latest/top-crop/width/150/height/150?cb=20160922163030")
-			embed.add_field(name="Calculs effectués avec :", value="\nPV : " + str(d) + " (dont 10k via les attirails)\nDéfense : " + str(b) + " (dont 1k via les attirails)\n\nEpv avec 3 Gemmes Pv : " + str(c), inline=False)
+			embed.add_field(name="Calculs effectués avec :", value="\nPV : " + str(d) + " (dont 10k via les attirails)\nDéfense : " + str(b) + " (dont 1k via les attirails)\n\n**Epv : " + str(c) + "**", inline=False)
 			await message.channel.send(embed=embed)
 		else:
 			await message.channel.send ("Erreur, Ecrivez 3GemPv suivi des PV de base et de la Def")
 	
-	
+	if ListElementInMessage[0] == "GemHeal":
+		if len(ListElementInMessage) == 3 and ListElementInMessage[1].isnumeric() and ListElementInMessage[2].isnumeric() and ListElementInMessage[3].isnumeric():
+			a = int(ListElementInMessage[1])
+			b = int(ListElementInMessage[2])+1000
+			c = int(ListElementInMessage[3])
+			d = round(((a * 1.68)+10000) / (1 - (((b * 1.68)+1000) / (((b * 1.68)+1000) + 1200))))
+			e = round(((a * 1.68)+10000) / (1 - (((b * 2.36)+1000) / (((b * 2.36)+1000) + 1200))))
+			f = round(((a * 1)+10000) / (1 - (b / (b + 1200))))
+			g = int((a*1.68)+10000)
+			h = int((c*2.36)+500)
+			embed=discord.Embed(title="Choix de Gemmes pour un healer", description="- Pv Def Recup \n- Pv/Def Recup x2 \n\nLes calculs sont effectués en prenant en compte 10kpv, 1kdef et 500recup via les trink")
+			embed.set_thumbnail(url="https://wiki.dungeondefenders2.com/images/6/6e/Heal.png")
+			embed.add_field(name="1 Pv, 1 Def, 1 Recup", value="1", inline=True)
+			embed.add_field(name="1 Pv, 2 Recup", value="1", inline=True)
+			embed.add_field(name="1 Def, 2 Recup", value="1", inline=True)
+			await ctx.send(embed=embed)
+			await message.channel.send(embed=embed)
+		else:
+			await message.channel.send ("Erreur, Ecrivez 3GemPv suivi des PV de base et de la Def")
 	
 	if ListElementInMessage[0] == "D":
 		if len(ListElementInMessage) == 4 and ListElementInMessage[1].isnumeric() and ListElementInMessage[2].isnumeric and ListElementInMessage[3].isnumeric:
@@ -92,7 +110,7 @@ async def on_message(message):
 			embed = discord.Embed(title="", color=0xffffff)
 			embed.set_thumbnail(url = "https://img2.pngio.com/markeus-b-ui-buttons-opengameartorg-attack-png-187_207.png")
 			embed.add_field(name="Calculs effectués avec : ", value="\nAttaque : " + str(a) + "\nTaux critique : " + str(b) + "\nDommages critiques : " + str(c) +
-					"\n\n__**Dégâts moyens**__ : "+ str(e)+ " (75k recomandé)" +
+					"\n\n__**Dégâts moyens__ : "+ str(e)+ "**" + " (75k recomandé)" +
 					"\n\n__Dégâts min :__ " + str(d)+ " (no crit)" +
 					"\n__Dégâts crit :__ "+ str(f)+ " (crit)",inline=False)
 			await message.channel.send(embed=embed)
