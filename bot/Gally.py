bea1ff2ae8c61c484e177a89d2fb2dca53cf0f67
horotopia <horotopia@hotmail.com>
2020-12-16 02:42:16 +0100

    Update Gally.py

--- bot/Gally.py
+++ bot/Gally.py
@@ -87,9 +87,10 @@ async def on_message(message):
 			d = round(a*5.5)
 			e = round(a*5.5*(1+(b/100)*(c/100)))
 			f = round(a*5.5*(1+(c/100)))
-			embed = discord.Embed(title="les Dmg de l'Astromon :", color=0xffffff)
+			embed = discord.Embed(title="Calculs effectués avec : ", color=0xffffff)
 			embed.set_thumbnail(url = "https://img2.pngio.com/markeus-b-ui-buttons-opengameartorg-attack-png-187_207.png")
-			embed.add_field(name="Calculs effectués avec : ", value="atk : "+ str(a) +"\nTc : "+ str(b) +"\nDc : "+ str(c) +"\n\nDégats sans crit : "+ str(d) +"\nDégats moyens : "+str(e)+"\nDégats crit : "+str(f),inline=False)
+			embed.add_field(name="", value="Atk : "+ str(a) +"\nTc : "+ str(b) +"\nDc : "+ str(c),inline=False)
+			embed.add_field(name="", value="\n__**Dégâts sans crit :**__ "+ str(d) +"\n\n__**Dégâts moyens__: "+str(e)+"**/75k (recomandé)\n__**Dégâts crit :**__ "+str(f),inline=False)
 			await message.channel.send(embed=embed)
 		else:
 			await message.channel.send ("Erreur, Ecrivez D suivi de l'attaque, du taux critique et des dégats critique")
