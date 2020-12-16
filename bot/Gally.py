0f55a71e7147cf6e7dab4872888291a410d00c47
horotopia <horotopia@hotmail.com>
2020-12-16 03:36:06 +0100

    Update Gally.py

--- bot/Gally.py
+++ bot/Gally.py
@@ -90,9 +90,9 @@ async def on_message(message):
 			embed = discord.Embed(title="", color=0xffffff)
 			embed.set_thumbnail(url = "https://img2.pngio.com/markeus-b-ui-buttons-opengameartorg-attack-png-187_207.png")
 			embed.add_field(name="Calculs effectués avec : ", value="\nAttaque : " + str(a) + "\nTaux critique : " + str(b) + "\nDommages critiques : " + str(c) +
-					"\n\n**Dégâts min :** " + str(d)+ " (no crit)" +
-					"\n__**Dégâts moyens**__ : "+ str(e)+ " (75k recomandé)" +
-					"\n**Dégâts crit :** "+ str(f)+ " (crit)",inline=False)
+					"\n\n__**Dégâts moyens**__ : "+ str(e)+ " (75k recomandé)" +
+					"\n\n__Dégâts min :__ " + str(d)+ " (no crit)" +
+					"\n__Dégâts crit :__ "+ str(f)+ " (crit)",inline=False)
 			await message.channel.send(embed=embed)
 		else:
 			await message.channel.send ("Erreur, Ecrivez D suivi de l'attaque, du taux critique et des dégats critique")
