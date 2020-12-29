f5452d3f97bd74ba67aca7362649d49c8d615316
horotopia <horotopia@hotmail.com>
2020-12-29 01:25:31 +0100

    Update Gally.py

--- bot/Gally.py
+++ bot/Gally.py
@@ -59,6 +59,26 @@ async def on_message(message):
 		else:
 			await message.channel.send("Ecrivez ePV suivi des PV et de la Def")
 
+########################################
+## Choisir ses gemmes (2 Def ou 2 Pv) ##
+########################################
+
+	ListElementInMessage = message.content.split()
+	if ListElementInMessage[0] == "EpvGem":
+		if len(ListElementInMessage) == 3 and ListElementInMessage[2].isnumeric():
+			a = int(ListElementInMessage[1])
+			b = int(ListElementInMessage[2])
+			c = round((a * 2.36) / (1 - ((b * 1.68) / ((b * 1.68) + 1200))))
+			d = round((a * 1.68) / (1 - ((b * 2.36) / ((b * 2.36) + 1200))))
+			embed = discord.Embed(title="", description=" ", url="", color=0xffffff)
+			embed.set_author(name=" ")
+			embed.set_thumbnail(url="")
+			embed.add_field(name="Calculs effectués avec:", value="\nPV de base: " + str(a) + "\nDéfense de base: " + str(b) + "\n\nEpv avec 2Pv + 1Def: " + str(c) + "\nEpv avec 1Pv + 2Def: " + str(d), inline=False)
+			await message.channel.send(embed=embed)
+
+		else:
+			await message.channel.send ("Erreur, Ecrivez EpvGem suivi des PV de base et de la Def de base")
+
 
 
 
