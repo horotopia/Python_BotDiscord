90cef8bc0531cb2d556d88684d2a67a411652c53
horotopia <horotopia@hotmail.com>
2020-12-29 01:40:17 +0100

    Update Gally.py

--- bot/Gally.py
+++ bot/Gally.py
@@ -12,6 +12,11 @@ client = discord.Client()
 async def on_message(message):
 	if message.author == client.user:
 		return
+########################################
+#            Split du message          #
+########################################
+
+	ListElementInMessage = message.content.split()
 
 ########################################
 ##############  Epv  ###################
@@ -20,52 +25,25 @@ async def on_message(message):
 #        d'oublie de la commande       #
 ########################################
 
-	if message.content.startswith("ePV "):
-		a = 0
-		b = 0
-		x = ""
-		invalid = False
-		spacerequired = True
-		info = message.content.replace("ePV ", "")
-		for i in range(0,len(info)):
-			if spacerequired == True and info[i] == " ":
-				spacerequired = False
-			elif list(info)[i] not in ["0","1","2","3","4","5","6","7","8","9"]:
-				invalid = True
-		if spacerequired == True:
-			invalid = True
-		if invalid == False:
-
-			for i in range(0,info.find(" ")):
-				x += info[i]
-
-			a = int(x)
-			x = ""
-
-			for i in range(info.find(" ") + 1, len(info)):
-				x += info[i]
-
-			b = int(x)
-
-			c = round(a/(1-(b/(b+1500))))
-
-			embed=discord.Embed(title="", description=" ", url="", color=0xffffff)
-			embed.set_author(name=" ")
-			embed.set_thumbnail(url="")
-			embed.add_field(name="Calculs effectués avec:", value="\nPV: " + str(a) +"\nDéfense: " + str(b) + "\n\n__**ePV**__: " + str(c), inline=False)
+	if ListElementInMessage[0] == "Epv":
+		if len(ListElementInMessage) == 3 and ListElementInMessage[1].isnumeric() and ListElementInMessage[2].isnumeric():
+			a = int(ListElementInMessage[1])
+			b = int(ListElementInMessage[2])
+			c = round(a/(1-(b/(b+1200))))
+			embed = discord.Embed(title="Epv", color=0xffffff)
+			embed.set_thumbnail(url="https://img.pngio.com/bar-development-game-health-video-game-icon-video-game-health-png-512_204.png")
+			embed.add_field(name="Calculs effectués avec:", value="\nPV : " + str(a) + "\nDéfense : " + str(b) + "\n\n**Epv : " + str(c) + "**", inline=False)
 			await message.channel.send(embed=embed)
-
-
+ 
 		else:
-			await message.channel.send("Ecrivez ePV suivi des PV et de la Def")
+			await message.channel.send ("Erreur, Ecrivez Epv suivi des PV et de la Def. (ex : Epv 20000 2000)")
 
 ########################################
 ## Choisir ses gemmes (2 Def ou 2 Pv) ##
 ########################################
 
-	ListElementInMessage = message.content.split()
 	if ListElementInMessage[0] == "EpvGem":
-		if len(ListElementInMessage) == 3 and ListElementInMessage[2].isnumeric():
+		if len(ListElementInMessage) == 3 and ListElementInMessage[1] and ListElementInMessage[2].isnumeric():
 			a = int(ListElementInMessage[1])
 			b = int(ListElementInMessage[2])
 			c = round((a * 2.36) / (1 - ((b * 1.68) / ((b * 1.68) + 1200))))
@@ -86,66 +64,23 @@ async def on_message(message):
 #        d'oublie de la commande       #
 ########################################
 
-	if message.content.startswith("D "):
-		a = 0
-		b = 0
-		c = 0
-		x = ""
-		y = ""
-		invalid = False
-		spacerequired = 2
-		info = message.content.replace("D ", "")
-		for i in range(0,len(info)):
-			if spacerequired > 0 and info[i] == " ":
-			spacerequired -= 1
-		elif list(info)[i] not in [".","0","1","2","3","4","5","6","7","8","9"]:
-			invalid = True
-		if spacerequired != 0:
-			invalid = True
-		if invalid == False:
-
-		for i in range(0,info.find(" ")):
-			x += info[i]
-			y += "x"
-		try:
-			a = float(x)
-		except:
-			invalid = True
-		x = ""
-		for i in range(info.find(" ") + 1, len(info)):
-			y += info[i]
-		for i in range(info.find(" ") + 1, y.find(" ") + 1):
-			x += info[i]
-		try:
-			b = float(x)
-		except:
-			invalid = True
-		x = ""
-
-		for i in range(y.find(" ") + 1, len(info)):
-			x += info[i]
-
-		try:
-			c = float(x)
-		except:
-			invalid = True
-
-		d = round(( 5.5 * a *( 1 + (b / 100 ) * (c/100))))
-
-		print(a,b,c)
-
-		if invalid == False:
-			embed=discord.Embed(title="", description=" ", url="", color=0xffffff)
-			embed.set_author(name=" ")
-			embed.set_thumbnail(url="")
-			embed.add_field(name="Calculs effectués avec:", value="\nAttaque: " + str(a) + "\nDommages critiques: " + str(b) + "\nTaux critique: " + str(c) + "\n\n__**Dégâts moyens**__: " + str(d) + "\n__**Dégâts Min**__: " + str(round(5.5 * a)) + " (no crit)\n__**Dégâts Max**__ : " + str(round(( 5.5 * a *( 1 + (b / 100 ) * (1)))))+ " (crit)", inline=False)
+	if ListElementInMessage[0] == "D":
+		if len(ListElementInMessage) == 4 and ListElementInMessage[1].isnumeric() and ListElementInMessage[2].isnumeric and ListElementInMessage[3].isnumeric:
+			a = int(ListElementInMessage[1])
+			b = int(ListElementInMessage[2])
+			c = int(ListElementInMessage[3])
+			d = round(a*5.5)
+			e = round(a*5.5*(1+(b/100)*(c/100)))
+			f = round(a*5.5*(1+(c/100)))
+			embed = discord.Embed(title="", color=0xffffff)
+			embed.set_thumbnail(url = "https://img2.pngio.com/markeus-b-ui-buttons-opengameartorg-attack-png-187_207.png")
+			embed.add_field(name="Calculs effectués avec : ", value="\nAttaque : " + str(a) + "\nTaux critique : " + str(b) + "\nDommages critiques : " + str(c) +
+					"\n\n__**Dégâts moyens__ : "+ str(e)+ "**" + " (75k recomandé)" +
+					"\n\n__Dégâts min :__ " + str(d)+ " (no crit)" +
+					"\n__Dégâts crit :__ "+ str(f)+ " (crit)",inline=False)
 			await message.channel.send(embed=embed)
 		else:
-			await message.channel.send("Ecrivez 'D' suivi de l'attaque, du dommage critique et du taux critique")
-		else:
-			await message.channel.send("Ecrivez 'D' suivi de l'attaque, du dommage critique et du taux critique")
-
-
+			await message.channel.send ("Erreur, Ecrivez D suivi de l'attaque, du taux critique et des dégats critique")
 
 ########################################
 ###########  Agression Def   ###########
