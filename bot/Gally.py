5a49afa7c3248decf6b021836738d0ea6d04dda5
horotopia <horotopia@hotmail.com>
2020-12-29 01:28:11 +0100

    Update Gally.py

--- bot/Gally.py
+++ bot/Gally.py
@@ -79,6 +79,117 @@ async def on_message(message):
 		else:
 			await message.channel.send ("Erreur, Ecrivez EpvGem suivi des PV de base et de la Def de base")
 
+########################################
+#############  Dégats   ################
+########################################
+#   mettre un message d'erreur en cas  #
+#        d'oublie de la commande       #
+########################################
+
+	if message.content.startswith("D "):
+		a = 0
+		b = 0
+		c = 0
+		x = ""
+		y = ""
+		invalid = False
+		spacerequired = 2
+		info = message.content.replace("D ", "")
+		for i in range(0,len(info)):
+			if spacerequired > 0 and info[i] == " ":
+			spacerequired -= 1
+		elif list(info)[i] not in [".","0","1","2","3","4","5","6","7","8","9"]:
+			invalid = True
+		if spacerequired != 0:
+			invalid = True
+		if invalid == False:
+
+		for i in range(0,info.find(" ")):
+			x += info[i]
+			y += "x"
+		try:
+			a = float(x)
+		except:
+			invalid = True
+		x = ""
+		for i in range(info.find(" ") + 1, len(info)):
+			y += info[i]
+		for i in range(info.find(" ") + 1, y.find(" ") + 1):
+			x += info[i]
+		try:
+			b = float(x)
+		except:
+			invalid = True
+		x = ""
+
+		for i in range(y.find(" ") + 1, len(info)):
+			x += info[i]
+
+		try:
+			c = float(x)
+		except:
+			invalid = True
+
+		d = round(( 5.5 * a *( 1 + (b / 100 ) * (c/100))))
+
+		print(a,b,c)
+
+		if invalid == False:
+			embed=discord.Embed(title="", description=" ", url="", color=0xffffff)
+			embed.set_author(name=" ")
+			embed.set_thumbnail(url="")
+			embed.add_field(name="Calculs effectués avec:", value="\nAttaque: " + str(a) + "\nDommages critiques: " + str(b) + "\nTaux critique: " + str(c) + "\n\n__**Dégâts moyens**__: " + str(d) + "\n__**Dégâts Min**__: " + str(round(5.5 * a)) + " (no crit)\n__**Dégâts Max**__ : " + str(round(( 5.5 * a *( 1 + (b / 100 ) * (1)))))+ " (crit)", inline=False)
+			await message.channel.send(embed=embed)
+		else:
+			await message.channel.send("Ecrivez 'D' suivi de l'attaque, du dommage critique et du taux critique")
+		else:
+			await message.channel.send("Ecrivez 'D' suivi de l'attaque, du dommage critique et du taux critique")
+
+
+
+########################################
+###########  Agression Def   ###########
+########################################
+#   mettre un message d'erreur en cas  #
+#        d'oublie de la commande       #
+########################################
+
+#faire un calcul d'agression def 
+#appel avec "AgrDef"
+
+########################################
+###########  Agression pv   ###########
+########################################
+#   mettre un message d'erreur en cas  #
+#        d'oublie de la commande       #
+########################################
+
+#faire un calcul d'agression pv 
+# appel avec "AgrPv"
+
+########################################
+###########  Agression Rec   ###########
+########################################
+#   mettre un message d'erreur en cas  #
+#        d'oublie de la commande       #
+########################################
+
+#faire un calcul d'agression recup 
+# Appel avec "AgrRec"
+
+########################################
+###############  heal   ################
+########################################
+#   mettre un message d'erreur en cas  #
+#        d'oublie de la commande       #
+########################################
+
+# faire un calcul de heal
+
+# recup du heal x 4 + recup du soigné x 2
+# demander la récup du heal + soigné
+
+
 
 
 
