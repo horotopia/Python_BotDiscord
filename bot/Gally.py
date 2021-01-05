c076e6f3e0027e0469016aff15f2c6c2158a12f7
horotopia <horotopia@hotmail.com>
2021-01-05 04:24:59 +0100

    Update Gally.py

--- bot/Gally.py
+++ bot/Gally.py
@@ -167,9 +167,17 @@ async def on_message(message):
 			await message.channel.send (CodeName +"\n"+ CodeUn +"\n"+ repr(CodeDeux)+"\n"+ CodeTexte)
 			
 	if ListElementInMessage[0] == "TestCode":
-#		NomMob = ListElementInMessage[1:3]
-#		Star = ListElementInMessage[3]
-#		ResteDuMessage = (ListElementInMessage[4:len(ListElementInMessage)])
+		NomMob = ListElementInMessage[1:3]
+		Star = ListElementInMessage[3]
+		ResteDuMessage = (ListElementInMessage[4:len(ListElementInMessage)])
+		
+		embed=discord.Embed(title="", url="", color=0xffffff)
+		embed.set_author(name="")
+		embed.set_thumbnail(url="")
+		embed.add_field(name=" ", value=ResteDuMessage, inline=False)
+
+
+	if ListElementInMessage[0] == "LightAlp"
 		embed=discord.Embed(title="", url="https://cdn.discordapp.com/attachments/552550283885019166/552559616010223628/StrangeAlpacaW_large.jpeg", color=0xffffff)
 		embed.set_author(name="#9 Alpaca (light)")
 		embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/552550283885019166/552559616010223628/StrangeAlpacaW_large.jpeg")
